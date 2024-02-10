d3b4af54caf246592e560f04466dc04e3427e003
aryo faizi <aryofaizi@gmail.com>
2024-02-10 10:57:34 +0330

    initialize_dynamic_settings implemented and attributes like bullet_speed and enemy_freqeuncy and enemy_speed can be valued by this method

--- settings.py
+++ settings.py
@@ -9,12 +9,22 @@ class Settings:
         self.jet_speed = 1.5
         self.bullet_width = 15
         self.bullet_height = 3
-        self.bullet_speed = 1.5
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
+        
+        # dynamic settings
+        self.initialize_dynamic_settings()
+        
+        # jet
+        self.jet_limit = 3
+        
+        # speed_up game
+        self.speedup_scale = 1.1
+        
+    def initialize_dynamic_settings(self):
+        """dynamic setting get initialized with this method"""
+        self.bullet_speed = 1.5
         # enemy position
         self.enemy_freqeuncy = 0.001
         self.enemy_speed = 0.5
         
-        # jet
-        self.jet_limit = 3
